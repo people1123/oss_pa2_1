8ca2a57537b543e4af376a3411ec7d77a8360602
nagunwoo <hard2818@gmail.com>
2024-06-05 15:50:36 +0900

    add background, block diagonal movement, quit event

--- frogger.py
+++ frogger.py
@@ -6,9 +6,10 @@ screen = pygame.display.set_mode((448,546),0,32)
 
 pygame.display.set_caption('Frogger')
 clock = pygame.time.Clock()
-
+background_filename = './images/background.png'
 frog_filename = './images/sprite_sheets_up.png'
 
+background = pygame.image.load(background_filename).convert()
 sprite_frog = pygame.image.load(frog_filename).convert_alpha()
 class Object():
     def __init__(self, position, sprite):
@@ -28,6 +29,7 @@ class Frog(Object):
         self.animation_counter = 0
         self.animation_tick = 1
         self.way = "UP"
+        self.can_move = 1
 
     def updateSprite(self, key_pressed):
         if self.way != key_pressed:
@@ -48,33 +50,39 @@ class Frog(Object):
 
     def moveFrog(self,key_pressed, key_up):
         print(self.animation_counter)
-        if self.animation_counter == 0:
+        if self.animation_counter == 0 :
             self.updateSprite(key_pressed)
         self.incAnimationCounter()
+        print("key up", key_up)
         if key_up == 1:
+            print(self.position[0], self.position[1])
             if key_pressed == "up":
-                if self.position[1] >39:
+                if self.position[1] > 39:
                     self.position[1] = self.position[1]-13
             elif key_pressed == "down":
-                if self.position[1] <473:
-                    self.position[1] = self.position[1]+13
+                if self.position[1] < 473:
+                        self.position[1] = self.position[1]+13
             if key_pressed == "left":
-                if self.position[0] >2:
-                    if self.animation_counter == 2:
+                if self.position[0] > 2:
+                    if self.animation_counter == 2 :
                         self.position[0] = self.position[0]-13
                     else:
                         self.position[0] = self.position[0]-14
             elif key_pressed == "right":
-                if self.position[0] <401:
+                if self.position[0] < 401:
                     if self.animation_counter == 2 :
                         self.position[0] = self.position[0]+13
                     else:
                         self.position[0] = self.position[0]+14
 
+    def cannotMove(self):
+        self.can_move = 0
+
     def animateFrog(self, key_pressed, key_up):
         if self.animation_counter != 0:
-            if self.animation_tick <=0:
+            if self.animation_tick <= 0:
                 self.moveFrog(key_pressed, key_up)
+                print("key: ",key_pressed)
                 self.animaiton_tick = 1
             else:
                 self.animation_tick = self.animation_tick -1
@@ -90,24 +98,27 @@ class Frog(Object):
             self.can_move = 1
 
 
+screen.blit(background, (0,0))
 frog = Frog([207,475], sprite_frog)
 key_up = 1
 key_pressed=0
 while True:
-    key_pressed=0
     
     
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                exit()
         if event.type == pygame.KEYUP:
             key_up = 1
         if event.type == pygame.KEYDOWN:
-            if key_up == 1:
+            if key_up == 1 and frog.can_move == 1:
                 key_pressed = pygame.key.name(event.key)
                 frog.moveFrog(key_pressed, key_up)
+                frog.cannotMove()
         
-    screen.fill((255,255,255))
+    screen.blit(background, (0,0))
     frog.animateFrog(key_pressed, key_up)
     frog.draw()
 
     pygame.display.update()
-    time_passed = clock.tick(5)
+    time_passed = clock.tick(30)
